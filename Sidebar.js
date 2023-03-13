1bf6ccf0306fe7cb35ea17ac77b40b551118914a
Alexandre Novaes <ale.oliveira00@gmail.com>
2023-03-13 19:47:53 -0300

    função gerar senha

---
+++ Sidebar.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const chars = "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+
+    let password = ""
+
+    for (let i = 0; i < 8; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber + 1)
+
+    }
+};
+generatePassword()
