7454384f4c532dd8ce8debdae82de576c885292e
Alexandre Novaes <ale.oliveira00@gmail.com>
2023-03-13 20:29:55 -0300

    cmt

--- Sidebar.js
+++ Sidebar.js
@@ -1,19 +1,21 @@
+//Função gerando senha aleatoria, em um lupin.
 function generatePassword() {
     const chars = "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
 
     let password = ""
-
+    
     for (let i = 0; i < 8; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber + 1)
 
     }
-    const inputEL = document.querySelector("#password")
+    //Adicionando a função de gerar senha no input.
+    const inputEL = document.querySelector("#password") 
     inputEL.value = password
 
     console.log(password)
 };
-
+//Função do input RANGE.
 const passwordLengthEL = document.querySelector("#password-length")
 passwordLengthEL.addEventListener("input", function () {
     const passwordLength = passwordLengthEL.value
